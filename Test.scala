a5a1a58bd671dc80d59e7c6799add91ab7254c2c
Sean Lee <freshdried@gmail.com>
2016-08-05 12:06:35 -0400

    updated

--- Test.scala
+++ Test.scala
@@ -1,13 +1,7 @@
 package com.originate.tododrop
 
-// comment
 class Test {
-  // TODO: add implementation
   def test: String = ???
-  // TODO: testtttt
-  // TODO: hohohohoo
-  // TODO: Another todo!
-
-
-  // Here's a fresh one!
 }
+
+// TODO: hotness
